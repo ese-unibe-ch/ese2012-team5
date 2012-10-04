ad82087068eeab2ec0620115896f1e8131d48625
Oliver Stapleton <oliver.stapleton@students.unibe.ch>
2012-10-04 16:39:21 +0200

    Placed model classes in subfolder (module) /marketplace .

--- app/models/marketplace/item.rb
+++ app/models/marketplace/item.rb
@@ -1,35 +1,37 @@
-class Item
+module Marketplace
+  class Item
 
-  # every item has a id
-  # every item has a owner
-  # items contains all items
+    # every item has a id
+    # every item has a owner
+    # items contains all items
 
-  @@items = Array.new
+    @@items = Array.new
 
-  attr_accessor :id, :name, :price, :owner, :active
+    attr_accessor :id, :name, :price, :owner, :active
 
 
-  def create(name, price, owner)
+    def create(name, price, owner)
+    end
+
+    def initialize
+    end
+
+    def self.by_id(id)
+    end
+
+    def self.all
+      @@items
+    end
+
+    def activate
+    end
+
+    def deactivate
+    end
+
+    def to_s
+      "Name: #{name} Price:#{self.price} Active:#{self.active} Owner:#{self.owner.name}"
+    end
+
   end
-
-  def initialize
-  end
-
-  def self.by_id(id)
-  end
-
-  def self.all
-    @@items
-  end
-
-  def activate
-  end
-
-  def deactivate
-  end
-
-  def to_s
-    "Name: #{name} Price:#{self.price} Active:#{self.active} Owner:#{self.owner.name}"
-  end
-
 end
