cdf0b6582f752666daf3be946317b4b84859e359
Oliver Stapleton <oliver.stapleton@students.unibe.ch>
2012-10-08 18:09:51 +0200

    added usage of BCrypt gem that takes care of password encryption.

--- app/controllers/authentication.rb
+++ app/controllers/authentication.rb
@@ -12,22 +12,22 @@ class Authentication < Sinatra::Application
     password = params[:password]
     user = Marketplace::User.by_name(username)
 
-    # check for any empty input or username don't exist
+    # check for any empty input or non-existent user
     if username == "" or password == ""
-      session[:message] = "empty username or password"
+      session[:message] = "empty username or password - login failed!"
       redirect "/login"
       elsif user.nil?
-      session[:message] = "username don't exists"
+      session[:message] = "user doesn't exist - login failed!"
       redirect "/login"
     end
 
-    # check password
-    real_password = BCrypt::Password.new(user.password)
-    if password == real_password
-      session[:name] = name
+    # Check password. Compares user input with hashed password via == method. Doesn't compare in plain text!
+    proper_password = BCrypt::Password.new(user.password)
+    if proper_password == password
+      session[:name] = username
       redirect "/"
     else
-      session[:message] = "login failed"
+      session[:message] = "wrong password - try again!!"
       redirect "/login"
     end
   end
@@ -49,18 +49,26 @@ class Authentication < Sinatra::Application
   post "/register" do
     username = params[:username]
     password = params[:password]
-    existing_user = Marketplace::User.by_name(username)
+    password_conf = params[:password_conf]
 
     if username_taken?(username)
       session[:message] = "username already in use"
       redirect "/register"
     end
 
+    if !password==password_conf
+      session[:message] = "password and confirmation don't match"
+      redirect "/register"
+    end
+
     new_user = Marketplace::User.create(username)
+
+
+    #caution: attr password [String] needs to be added to Marketplace::User.
     new_user.password = BCrypt::Password.create(password)
     new_user.save()
 
-    session[:message] = "#{new_user.name} created, please log in"
+    session[:message] = "#{new_user.name} created, now log in"
     redirect "/login"
   end
 
