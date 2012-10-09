e7dc22a2b5001e30a2f0c0c030f7515ac15332bd
daniel-wuethrich <daniel.wuethrich@students.unibe.ch>
2012-10-09 21:21:24 +0200

    buy and activation is implemented

--- app/controllers/item.rb
+++ app/controllers/item.rb
@@ -1,7 +1,6 @@
 class Item < Sinatra::Application
 
 
-
   #link to correct item page
   get "/item/:id" do
     current_item = Marketplace::Item.by_id(params[:id].to_i)
@@ -19,13 +18,41 @@ class Item < Sinatra::Application
   post "/item/:id" do
     current_item = Marketplace::Item.by_id(params[:id].to_i)
 
+
+    #only possible action while activity
+
     #check if a session is in progress and if the current user is owner
     if session[:name] == current_item.owner.name
+
+
+      #allows to activate and deactivate item
+      if params[:activation] == 'deactivate'
+        current_item.deactivate
+      end
+
+      if params[:activation] == 'activate'
+        current_item.activate
+      end
+
+
       haml :own_item_profile, :locals => {:items => Marketplace::Item.all,
                                           :item => current_item}
     else
-      haml :item_profile, :locals => {:items => Marketplace::Item.all,
-                                      :item => current_item}
+
+
+      actualUser = Marketplace::User.by_name(session[:name])
+
+      actualUser.buy(current_item)
+
+      if actualUser.name == current_item.owner.name
+        haml :own_item_profile, :locals => {:items => Marketplace::Item.all,
+                                            :item => current_item}
+      else
+
+
+        haml :item_profile, :locals => {:items => Marketplace::Item.all,
+                                        :item => current_item}
+      end
     end
   end
 end
