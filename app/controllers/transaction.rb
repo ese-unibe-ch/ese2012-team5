76774680ed2abd99862b11e021adbd870f00558c
daniel-wuethrich <daniel.wuethrich@students.unibe.ch>
2012-10-09 23:11:13 +0200

    editing, buying and (de)activating works

--- app/controllers/transaction.rb
+++ app/controllers/transaction.rb
@@ -1,3 +1,27 @@
 class Transaction < Sinatra::Application
-  # To change this template use File | Settings | File Templates.
+  get "/edit/:id" do
+    current_item = Marketplace::Item.by_id(params[:id].to_i)
+
+
+    haml :edit_item, :locals => {:items => Marketplace::Item.all,
+                                 :item => current_item}
+
+  end
+
+
+  post "/edit/:id" do
+    current_item = Marketplace::Item.by_id(params[:id].to_i)
+
+
+    current_item.name=params[:newName]
+
+    current_item.price=params[:newPrice]
+
+
+    haml :edit_item, :locals => {:items => Marketplace::Item.all,
+                                 :item => current_item}
+
+
+    redirect "/"
+  end
 end
