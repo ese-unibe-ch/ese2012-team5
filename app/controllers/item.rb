da1c12abb0e58953bf125ecb527b8d9914fa121f
daniel-wuethrich <daniel.wuethrich@students.unibe.ch>
2012-10-09 12:31:39 +0200

    user can activate and deaktivate item, buy and sell items

--- app/controllers/item.rb
+++ app/controllers/item.rb
@@ -1,3 +1,33 @@
+require 'rubygems'
+require 'sinatra'
+require 'tilt/haml'
+
+require '../app/models/marketplace/user'
+require '../app/models/marketplace/item'
 class Item < Sinatra::Application
   # To change this template use File | Settings | File Templates.
+    get "/items"  do
+
+
+      items= Marketplace::Item.all
+      actualUser = Marketplace::User.by_name(session[:name])
+      haml :item   , :locals => { :items => items, user => actualUser}
+
+    end
+
+    post "/items"  do
+
+
+      items= Marketplace::Item.all
+
+      itemToBuy = Marketplace::Item.by_id(Integer(params[:Item]))
+
+      actualUser.buy(itemToBuy)
+
+      haml :item   , :locals => { :items => items, user => actualUser}
+
+    end
+
+
+
 end
--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -1,3 +1,45 @@
+require 'rubygems'
+require 'sinatra'
+require 'tilt/haml'
+
+require '../app/models/marketplace/user'
+require '../app/models/marketplace/item'
+
+
 class User < Sinatra::Application
   # To change this template use File | Settings | File Templates.
+  get "/users" do
+
+    #session[:name] = 'Daniel'
+
+
+    actualUser = Marketplace::User.by_name(session[:name])
+
+
+    haml :user, :locals => {:user => actualUser}
+
+  end
+
+
+  post "/users" do
+
+    #session[:name] = params['Daniel']
+
+
+    actualUser = Marketplace::User.by_name(session[:name])
+
+    item = Marketplace::Item.by_id(Integer(:Item))
+
+
+    if item.active
+      item.deactivate
+    else
+      item.activate
+    end
+
+
+    haml :user, :locals => {:user => actualUser}
+
+  end
+
 end
