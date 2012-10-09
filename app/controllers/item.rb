7eb7927ebe2839421a728179675abe4e178c137d
daniel-wuethrich <daniel.wuethrich@students.unibe.ch>
2012-10-09 20:00:53 +0200

    Buy botton

--- app/controllers/item.rb
+++ app/controllers/item.rb
@@ -1,29 +1,22 @@
 class Item < Sinatra::Application
 
-  get "/items" do
 
-
-    items= Marketplace::Item.all
-    actualUser = Marketplace::User.by_name(session[:name])
-    haml :item, :locals => {:items => items, user => actualUser}
-
-  end
-
-  post "/items" do
-
-
-    items= Marketplace::Item.all
-
-    itemToBuy = Marketplace::Item.by_id(Integer(params[:Item]))
-
-    actualUser.buy(itemToBuy)
-
-    haml :item, :locals => {:items => items, user => actualUser}
-
-  end
 
   #link to correct item page
   get "/item/:id" do
+    current_item = Marketplace::Item.by_id(params[:id].to_i)
+
+    #check if a session is in progress and if the current user is owner
+    if session[:name] == current_item.owner.name
+      haml :own_item_profile, :locals => {:items => Marketplace::Item.all,
+                                          :item => current_item}
+    else
+      haml :item_profile, :locals => {:items => Marketplace::Item.all,
+                                      :item => current_item}
+    end
+  end
+
+  post "/item/:id" do
     current_item = Marketplace::Item.by_id(params[:id].to_i)
 
     #check if a session is in progress and if the current user is owner
