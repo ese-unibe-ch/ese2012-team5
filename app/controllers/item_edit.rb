e26254469fc0c1accd64ad782944b0c7788d6198
Urs Zysset <UrsZysset@gmail.com>
2012-10-11 10:22:45 +0200

    fixed that NaN prices are accepted as new itemprice

--- app/controllers/item_edit.rb
+++ app/controllers/item_edit.rb
@@ -1,43 +1,50 @@
 class ItemEdit < Sinatra::Application
 
   get "/edit/:id" do
+    #get data from params
     id = params[:id].to_i
 
     current_item = Marketplace::Item.by_id(id)
 
+    #go back if its active
     if(current_item.active)
       redirect "/item/#{id}"
     end
 
     message = session[:message]
     session[:message] = nil
-    haml :edit_item, :locals => {:items => Marketplace::Item.all,
-                                 :item => current_item,
-                                  :info => message}
-
+    haml :edit_item, :locals => { :items => Marketplace::Item.all,
+                                  :item => current_item,
+                                  :info => message                }
   end
 
 
   post "/edit/:id" do
-
+    #get data from params
     id = params[:id].to_i
     new_name = params[:name]
-    new_price = params[:price].to_i
+    new_price = params[:price]
 
     current_item = Marketplace::Item.by_id(id)
 
+    #check if the name is valid
     if(new_name == nil or new_name == "")
       session[:message] = "empty name!"
       redirect "/edit/#{id}"
     end
 
-    if(!(new_price.is_a? Integer))
-      session[:message] = "price was not a number!"
-      redirect "/edit/#{id}"
+    #check if the price is valid
+    begin
+      !(Integer(new_price))
+
+      rescue ArgumentError
+        session[:message] = "price was not a number!"
+        redirect "/edit/#{id}"
     end
 
+    #overwrite data
     current_item.name = new_name
-    current_item.price = new_price
+    current_item.price = new_price.to_i
 
     redirect "/item/#{id}"
   end
