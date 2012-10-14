1c375da988c31994bb794bd13158935abcea8c27
daniel-wuethrich <daniel.wuethrich@students.unibe.ch>
2012-10-14 13:14:37 +0200

    workin with money controll

--- app/controllers/itemActions.rb
+++ app/controllers/itemActions.rb
@@ -4,19 +4,24 @@ class ItemActions < Sinatra::Application
   #link to correct item page
   get "/item/:id" do
     current_item = Marketplace::Item.by_id(params[:id].to_i)
-
+    current_user = Marketplace::User.by_name(session[:name])
     #check if a session is in progress and if the current user is owner
     if session[:name] == current_item.owner.name
       haml :item_profile_own, :locals => {:items => Marketplace::Item.all,
                                           :item => current_item}
     else
 
+      if current_user
+        userCredits  = current_user.credits
+      else
+        userCredits=0
+      end
 
       haml :item_profile, :locals => {:items => Marketplace::Item.all,
-                                      :item => current_item}
+                                      :item => current_item,
+                                      :userCredits => userCredits}
     end
   end
-
 
 
 end
