7507d9928da3e6c6a61a132eb7428851bed4a9da
Oliver Stapleton <oliver.stapleton@students.unibe.ch>
2012-10-15 22:03:06 +0200

    Increased username validation. Usernames now have a min and max length.

--- app/controllers/register.rb
+++ app/controllers/register.rb
@@ -11,12 +11,9 @@ class Register < Sinatra::Application
     password = params[:password]
     password_conf = params[:password_conf]
 
-    if username_taken?(username)
-      session[:message] = "username already in use"
-      redirect '/register'
-    end
+    validate_username(username, 3, 12)
 
-    validate(password, password_conf, 4)
+    validate_password(password, password_conf, 4)
 
     new_user = Marketplace::User.create(username, password)
     new_user.save()
@@ -25,17 +22,30 @@ class Register < Sinatra::Application
     redirect '/login'
   end
 
-  #checks if username is already in use
+  #checks if username is already in use and if length of username is within the given limits
   # @param [String] username name to check
-  def username_taken?(username)
-    nil != Marketplace::User.by_name(username)
+  # @param [Integer] min minimal length username must have
+  # @param [Integer] max maximal length username can have
+  def validate_username(username, min, max)
+    if Marketplace::User.by_name(username)
+      session[:message] = "username already taken"
+      redirect '/register'
+    end
+    if username.length<3
+      session[:message] = "username too short"
+      redirect '/register'
+    end
+    if username.length>12
+      session[:message] = "username too long"
+      redirect '/register'
+    end
   end
 
   #validates password input by user.
   # @param [String] password user chooses
   # @param [String] password_conf password confirmation
   # @param [Integer] length minimal length in characters password must have
-  def validate(password, password_conf, length)
+  def validate_password(password, password_conf, length)
     if password != password_conf
       session[:message] = "password and confirmation don't match"
       redirect '/register'
