019cbba4839248613a46eaff004ef30ce738c783
Urs Zysset <UrsZysset@gmail.com>
2012-10-16 14:06:01 +0200

    quantity not editable anymor and minor layout corrections

--- app/controllers/item_edit.rb
+++ app/controllers/item_edit.rb
@@ -32,7 +32,6 @@ class ItemEdit < Sinatra::Application
     id = params[:id].to_i
     new_name = params[:name]
     new_price = params[:price]
-    new_quantity = params[:quantity]
     current_item = Marketplace::Item.by_id(id)
     current_user = Marketplace::User.by_name(session[:name])
 
@@ -49,22 +48,8 @@ class ItemEdit < Sinatra::Application
       redirect "/item/#{id}/edit"
     end
 
-    begin
-      !(Integer(new_quantity))
-    rescue ArgumentError
-      session[:message] = "quantity was not a number!"
-      redirect "/item/#{id}/edit"
-    end
-
-    if new_quantity.to_i <= 0
-      session[:message] = "quantity must be bigger than 0"
-      redirect "/item/#{id}/edit"
-    end
-
-
     current_item.name = new_name
     current_item.price = new_price.to_i
-    current_item.quantity = new_quantity.to_i
 
     # Check if the creator already owns a similar item, do we need to merge these items?
     need_merge = false
