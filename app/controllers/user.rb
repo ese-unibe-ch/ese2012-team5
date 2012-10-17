608fc3c3a5e6b51cb41bb34beb26ff02ea076d6b
Oliver Stapleton <oliver.stapleton@students.unibe.ch>
2012-10-17 15:15:03 +0200

    Polished previous commit with the help of @uzysset

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -6,19 +6,14 @@ class User < Sinatra::Application
     message = session[:message]
     session[:message] = nil
     user = Marketplace::User.by_name(username)
-    details = user.details.gsub(/\n/, '<br />')
-
-    #the gsub() method changes textarea-style linebreaks to html-style linebreaks
 
     if username == session[:name]
       haml :user_profile_own, :locals => {  :user => user,
-                                            :info => message,
-                                            :details => details}
+                                            :info => message}
     else
       haml :user_profile, :locals => {  :user => user,
                                         :current_user => Marketplace::User.by_name(session[:name]),
-                                        :info => message,
-                                        :details => details}
+                                        :info => message}
     end
 
   end
