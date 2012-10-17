65335e0f59000c2705ceb5b222f205b164bbae8b
Oliver Stapleton <oliver.stapleton@students.unibe.ch>
2012-10-17 14:47:01 +0200

    Linebreaks in interest form now aren't ignored in profile view.

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -5,14 +5,20 @@ class User < Sinatra::Application
     username = params[:name]
     message = session[:message]
     session[:message] = nil
+    user = Marketplace::User.by_name(username)
+    details = user.details.gsub(/\n/, '<br />')
+
+    #the gsub() method changes textarea-style linebreaks to html-style linebreaks
 
     if username == session[:name]
-      haml :user_profile_own, :locals => {  :user => Marketplace::User.by_name(username),
-                                            :info => message}
+      haml :user_profile_own, :locals => {  :user => user,
+                                            :info => message,
+                                            :details => details}
     else
-      haml :user_profile, :locals => {  :user => Marketplace::User.by_name(username),
+      haml :user_profile, :locals => {  :user => user,
                                         :current_user => Marketplace::User.by_name(session[:name]),
-                                        :info => message}
+                                        :info => message,
+                                        :details => details}
     end
 
   end
