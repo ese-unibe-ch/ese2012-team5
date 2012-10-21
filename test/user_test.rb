fa28ef23dad120d1017487a44464dfdb077be69c
Reencode <sudojudo@eml.cc>
2012-10-21 21:14:21 +0200

    updated test .. still needs some work

--- test/user_test.rb
+++ test/user_test.rb
@@ -1,93 +1,102 @@
 require 'test/unit'
+require 'rubygems'
+require 'bcrypt'
 require '../app/models/marketplace/item.rb'
 require '../app/models/marketplace/user.rb'
+require '../app/models/marketplace/database.rb'
 
 # syntax for inheritance
-class ItemTest < Test::Unit::TestCase
+class UserTest < Test::Unit::TestCase
 
   def test_list
-    owner = Marketplace::User.create('John')
-    item = Marketplace::Item.create('The Lord of The Rings, Books', 10, owner)
+    database = Marketplace::Database.instance
+    owner = Marketplace::User.create('John','pW123')
+    item = Marketplace::Item.create('The Lord of The Rings, Books', 10, 12, owner)
     item.activate
-    assert(owner.items_to_sell.include?(item), "List should include active items" )
-    item.deactivate
-    assert(owner.items_not_to_sell.include?(item), "List should include inactive items" )
-    assert(!owner.items_to_sell.include?(item), "List should not include inactive items" )
+    assert(owner.items.include?(item), "List should include active items" )
   end
 
   def test_username
-    owner = Marketplace::User.create('John')
+    database = Marketplace::Database.instance
+    owner = Marketplace::User.create('John','pW123')
     assert(owner.name == 'John', "Username")
   end
 
   def test_credits
-    owner = Marketplace::User.create('John')
+    database = Marketplace::Database.instance
+    owner = Marketplace::User.create('John','pW123')
     assert(!(owner.credits==0), "User should have some initial credits")
   end
 
   def test_has_bigger_amount
-    user = Marketplace::User.create("Heinro")
+    database = Marketplace::Database.instance
+    user = Marketplace::User.create("Heinro",'pW123')
     user.credits += 50
     assert(user.credits == 150, "credit is not 150!")
   end
 
   def test_has_smaller_amount
-    user = Marketplace::User.create("Ulrich")
+    database = Marketplace::Database.instance
+    user = Marketplace::User.create("Ulrich",'pW123')
     user.credits -= 50
     assert(user.credits == 50, "credit is not 50!")
   end
 
   def test_initialCredits
-    owner = Marketplace::User.create('John')
+    database = Marketplace::Database.instance
+    owner = Marketplace::User.create('John','pW123')
     assert(owner.credits==100, "User should have 100 initial credits")
   end
 
   def test_add_item
+    database = Marketplace::Database.instance
     # list is empty now
-    owner = Marketplace::User.create("Serioso")
+    owner = Marketplace::User.create("Serioso","pW123")
     assert(owner.items.length == 0, "list should be empty empty")
-    itemX = Marketplace::Item.create("itemX",300,owner)
+    itemX = Marketplace::Item.create("itemX",300,21,owner)
     owner.add_item(itemX)
     assert(owner.items.include?(itemX), "item was not added!")
-    assert(owner.items_not_to_sell.include?(itemX), "List should include inactive items" )
-    assert(!owner.items_to_sell.include?(itemX), "List should not include inactive items" )
   end
 
   def test_buy_item_inactive
-    seller = Marketplace::User.create('John')
-    buyer = Marketplace::User.create('Jack')
-    item = Marketplace::Item.create('The Lord of The Rings, Books', 10, seller)
+    database = Marketplace::Database.instance
+    seller = Marketplace::User.create('John','pW123')
+    buyer = Marketplace::User.create('Jack','pW123')
+    item = Marketplace::Item.create('The Lord of The Rings, Books', 10,1, seller)
     item.active= true
     buyer.buy(item)
     assert(!item.active, "Item should be inactive")
   end
 
   def test_not_enough_credits
-    seller = Marketplace::User.create('John')
-    buyer = Marketplace::User.create('Jack')
-    item = Marketplace::Item.create('The Lord of The Rings, Books', 101, seller)
+    database = Marketplace::Database.instance
+    seller = Marketplace::User.create('John','pW123')
+    buyer = Marketplace::User.create('Jack','pW123')
+    item = Marketplace::Item.create('The Lord of The Rings, Books', 101, 1,seller)
     item.active= true
-    buyer.buy(item)
+    assert_raise(NameError){user.buy(item)}
     assert(seller.items.include?(item), "Item should still be on the sellers list")
     assert(buyer.credits == 100, "Credits should not change")
     assert(seller.credits == 100, "Credits should not change")
   end
 
   def test_not_active
-    seller = Marketplace::User.create('John')
-    buyer = Marketplace::User.create('Jack')
-    item = Marketplace::Item.create('The Lord of The Rings, Books', 10, seller)
+    database = Marketplace::Database.instance
+    seller = Marketplace::User.create('John','pW123')
+    buyer = Marketplace::User.create('Jack','pW123')
+    item = Marketplace::Item.create('The Lord of The Rings, Books', 10,1, seller)
     item.active= false
-    buyer.buy(item)
+    assert_raise(NameError){user.buy(item)}
     assert(seller.items.include?(item), "Item should still be on the sellers list")
     assert(buyer.credits == 100, "Credits should not change")
     assert(seller.credits == 100, "Credits should not change")
   end
 
   def test_become_owner_at_trade
-    user = Marketplace::User.create("Buyer")
-    owner = Marketplace::User.create("Owner")
-    item = Marketplace::Item.create("someItem", 100, owner)
+    database = Marketplace::Database.instance
+    user = Marketplace::User.create("Buyer",'pW123')
+    owner = Marketplace::User.create("Owner",'pW123')
+    item = Marketplace::Item.create("someItem", 100,1, owner)
     item.active= true
     user.buy(item)
     assert(user.items.size == 1, "user was not able to buy!")
@@ -95,9 +104,10 @@ class ItemTest < Test::Unit::TestCase
   end
 
   def test_transfer_amount_at_trade
-    user = Marketplace::User.create("Buyer")
-    owner = Marketplace::User.create("Owner")
-    item = Marketplace::Item.create("normalItem",100,owner)
+    database = Marketplace::Database.instance
+    user = Marketplace::User.create("Buyer",'pW123')
+    owner = Marketplace::User.create("Owner",'pW123')
+    item = Marketplace::Item.create("normalItem",100,1,owner)
     item.active= true
     user.buy(item)
     assert(user.credits == 0, "user has too much credit!")
@@ -105,20 +115,21 @@ class ItemTest < Test::Unit::TestCase
   end
 
   def test_removes_from_user
-    user = Marketplace::User.create("Buyer")
-    owner = Marketplace::User.create("Owner")
-    item = Marketplace::Item.create("normalItem",100,owner)
+    database = Marketplace::Database.instance
+    user = Marketplace::User.create("Buyer",'pW123')
+    owner = Marketplace::User.create("Owner",'pW123')
+    item = Marketplace::Item.create("normalItem",100,1,owner)
     item.active= true
     user.buy(item)
     assert(owner.items.length == 0, "owner still has the item on his list!")
   end
 
   def test_fail_inactive
-    user = Marketplace::User.create("Buyer")
-    owner = Marketplace::User.create("Owner")
-    item = Marketplace::Item.create("Whoot",10,owner)
-    user.buy(item)
-    assert(user.items.size == 0, "user could have bought the inactive item!")
+    database = Marketplace::Database.instance
+    user = Marketplace::User.create("Buyer",'pW123')
+    owner = Marketplace::User.create("Owner",'pW123')
+    item = Marketplace::Item.create("Whoot",10,1,owner)
+    assert_raise(TypeError){user.buy(item)}
   end
 
 end
