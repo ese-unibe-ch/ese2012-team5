ed64afc55118b27cc4a006dac9dfd96024989174
Urs Zysset <UrsZysset@gmail.com>
2012-10-22 09:18:29 +0200

    bug fixed that guest couldn't open profilepages

--- app/controllers/user.rb
+++ app/controllers/user.rb
@@ -7,7 +7,7 @@ class User < Sinatra::Application
   get "/user/:name" do
 
     current_user = @database.user_by_name(session[:name])
-    current_items = current_user.items
+    current_items = current_user.items unless !session[:name]
     user = @database.user_by_name(params[:name])
     user_items = user.items
 
