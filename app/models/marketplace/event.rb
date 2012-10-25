5edef96c9c5104ac76e34ad1f5f69bba06bf0835
Oliver Stapleton <oliver.stapleton@students.unibe.ch>
2012-10-25 14:09:31 +0200

    added some comments and method structure of temp pw function

--- app/models/marketplace/event.rb
+++ app/models/marketplace/event.rb
@@ -2,6 +2,7 @@ module Marketplace
   require 'rubygems'
   require 'tlsmail'
 
+  #class of events, takes care of verification and password changing processes, both via email with generated link
   class Event
     attr_accessor :user
 
@@ -11,6 +12,8 @@ module Marketplace
       event
     end
 
+    #sends a verification email to the event's user from the item markets email acc
+    #todo mail must contain a link that when clicked, leads to a controller that sets the attribute 'verified' of the user from false to true
     def send_verification_mail
       acc = 'itemmarketemailclient@gmail.com'
       pw = 'itemmarket101'
@@ -19,6 +22,24 @@ module Marketplace
 
       content = <<EOF
 Hier sollte nun ein Verifizierungs-Link generiert werden!
+EOF
+
+      Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
+      Net::SMTP.start('smtp.gmail.com', 587, 'gmail.com', acc, pw, :login) do |smtp|
+        smtp.send_message(content, acc, email)
+      end
+    end
+
+    #sends a pw to the user that let's him log in without entering his forgotten password. temp pw should only be valid 24h.
+    #todo mail must contain a temp pw that when clicked, leads to a controller that logs in the user and prompts him to change pw immediately
+    def send_temp_pw_mail
+      acc = 'itemmarketemailclient@gmail.com'
+      pw = 'itemmarket101'
+      recipient = self.user
+      email = recipient.email
+
+      content = <<EOF
+Hier sollte nun ein temporäres Passwort verschickt werden
 EOF
 
       Net::SMTP.enable_tls(OpenSSL::SSL::VERIFY_NONE)
