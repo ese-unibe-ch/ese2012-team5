ad6644102e43cb42e0f91712189f4fbf6dd31529
Reencode <sudojudo@eml.cc>
2012-11-25 00:24:06 +0100

    search function version 1 may be buggy

--- app/controllers/item_search.rb
+++ app/controllers/item_search.rb
@@ -4,19 +4,54 @@ class Item_search
     @database = Marketplace::Database.instance
   end
 
-  # Hand over search word
-  get '/search_item' do
+  # Handle AJAX requests
 
-    query = params[:search_query]
+  get '/search_item/:for' do
+    query = params[:for].downcase
     current_user = @database.user_by_name(session[:name])
-    items = @database.categories_items_without(current_user)
-    sorted_items = @database.sort_categories_by_price(items)
+    active_items = @database.all_active_items
+    found_by_name = []
+    found_by_description = []
+
+    active_items.each { |item|
+      if item.name.downcase.include?(query)
+        found_by_name.push(item)
+      end
+    }
+
+    active_items.each { |item|
+      if item.description.downcase.include?(query)
+        found_by_description.push(item)
+        desc = item.description
+        start_of_find = desc.index(query)
+        substring_start = start_of_find-12<0 ? 0 : start_of_find-12
+        substring_end = start_of_find+15>desc.length ? desc.size : start_of_find+15
+
+        item.description_search = desc[substring_start..substring_end]
+
+        if substring_end != desc.size
+          item.description_search << "..."
+        end
+
+        if substring_start != 0
+          item.description_search = "..."+item.description_search
+        end
+
+      end
+    }
+
+    found_items = found_by_name || found_by_description
 
 
-    haml :item_search, :locals => { :search_query => query,
-                                    :items => items,
-                                    :current_user => current_user,
-                                    :categories => sorted_items}
+    categorized_found = @database.categories_given_without(found_items,current_user)
+    categorized_sorted_found = @database.sort_categories_by_price(categorized_found)
+
+
+    haml :item_search,:layout => false ,
+         :locals => {  :found_items => categorized_sorted_found,
+                       :current_user => current_user,
+         }
+
 
   end
 
