1282e3d8f4ea00eadb5122c057de7137ce61f664
Reencode <sudojudo@eml.cc>
2012-11-26 08:05:52 +0100

    somehow killed the no bug ^^ everything looks good now

--- app/controllers/item_search.rb
+++ app/controllers/item_search.rb
@@ -10,29 +10,28 @@ class Item_search
 
     query = params[:for].gsub(/_/," ").downcase
 
-
     current_user = @database.user_by_name(session[:name])
     active_items = @database.all_active_items
     found_by_name = []
     found_by_description = []
 
     active_items.each { |item|
-      if item.name.downcase.include?(query)
+      if item.name.gsub(/_/," ").downcase.include?(query)
         found_by_name.push(item)
       end
     }
 
     active_items.each { |item|
-      desc = item.description.gsub("/,/","_")
+      desc = item.description.gsub("/,/","~")
 
-      if desc.downcase.include?(query)
+      if desc.gsub(/_/," ").downcase.include?(query)
         found_by_description.push(item)
 
-        start_of_find = desc.index(query)
+        start_of_find = desc.gsub(/_/," ").downcase.index(query)
         substring_start = if start_of_find-17<0 then 0 else start_of_find-17 end
         substring_end = if start_of_find+20>desc.size then desc.size else start_of_find+20 end
 
-        item.description_search = desc[substring_start..substring_end].gsub("/_/",",")
+        item.description_search = desc[substring_start..substring_end].gsub("/~/",",")
 
         if substring_end != desc.size
           item.description_search << "..."
