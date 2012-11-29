9a034f4aa45cf8f965466bf1da6518138762685b
Lukas von Rotz <luky_von_rotz@hotmail.com>
2012-11-29 16:01:03 +0100

    last bug fixed :)

--- app/controllers/item_create.rb
+++ app/controllers/item_create.rb
@@ -41,7 +41,8 @@ class ItemCreate < Sinatra::Application
 
     new_item = Marketplace::Item.create(name, price.to_i, quantity.to_i, current_user)
     time_now = Time.new
-    # Add new description and price into the description log
+    # Add new description into log and delete standard entry
+    new_item.description_log.clear
     new_item.add_description(time_now,description,price.to_i)
 
     session[:message] = "~note~you have created #{new_item.name}"
