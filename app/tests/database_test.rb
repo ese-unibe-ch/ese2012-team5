f4342a2355e46991889135766c155e155b8db2a0
unknown <danielwue@hotmail.com>
2012-12-11 18:03:47 +0100

    more database and search tests

--- app/tests/database_test.rb
+++ app/tests/database_test.rb
@@ -32,23 +32,21 @@ class Database_Tests < Test::Unit::TestCase
     assert(database.all_users.include?(user),"User not included")
     #the database gives the right user back when searching for names
     assert(database.user_by_name('John2')!=user)
-
+    assert(database.email_exists?('test@testmail1.com'))
     user1 = database.user_by_email('test@testmail1.com')
-
+    #should have as many email addresses as user
+    assert(database.all_emails.size,8)
     users=database.all_users
     #all user are added to the singleton database
-    assert_equal(users.size,7)
+    assert_equal(users.size,8)
 
 
 
     database.delete_user(user1)
-
     database.delete_deactivated_user(user1)
-
     users=database.all_users
-
     #a user is removed
-    assert_equal(users.size,6)
+    assert_equal(users.size,7)
 
 
 
@@ -129,5 +127,28 @@ class Database_Tests < Test::Unit::TestCase
     assert(user.verified == true)
 
   end
+
+  def test_password_reset
+    database = Marketplace::Database.instance
+    user = Marketplace::User.create('John','pW123','test@testmail1.com')
+    timestamp = Time.new - 7201
+    hash = "asjhakfad12lj3lkehkf2342h3hk4j"
+
+    #reset pw can be called and removed by hash
+    database.add_pw_reset(hash,user,timestamp)
+    assert(database.pw_reset_has?(hash))
+    database.delete_pw_reset(hash)
+    assert(!database.pw_reset_has?(hash))
+
+    #reset pw can expire because of the time
+    database.add_pw_reset(hash,user,timestamp)
+    assert(database.pw_reset_has?(hash))
+    database.clean_pw_reset_older_as(2)
+    assert(!database.pw_reset_has?(hash))
+
+
+  end
+
+
 
 end
