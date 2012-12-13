d17a47ca7eab572bf2f680bbddb409486cc396d8
Urs Zysset <UrsZysset@gmail.com>
2012-12-13 08:43:51 +0100

    thats how it should look like danielw..

--- tests/controller_tests/controller_login_test.rb
+++ tests/controller_tests/controller_login_test.rb
@@ -1,58 +1,63 @@
 require 'rubygems'
 require 'sinatra'
+require 'test/unit'
 require 'bcrypt'
 require 'tilt/haml'
 require 'webget_ruby_secure_random'
 require 'tlsmail'
 require 'require_relative'
-require '../../app/controllers/login.rb'
-require '../../app/controllers/main.rb'
+require 'selenium/webdriver'
 
-
-require "test/unit"
-require 'require_relative'
-require "rubygems"
-require "selenium/webdriver"
+require_relative '../../app/controllers/login.rb'
+require_relative '../../app/controllers/main.rb'
 
 
 class Controller_Login_Test <Test::Unit::TestCase
 
-def setup
-  @driver = Selenium::WebDriver.for :firefox
-  @driver.get("localhost:4567")
-end
-def teardown
-  @driver.quit
-end
+  def setup
+    @driver = Selenium::WebDriver.for :firefox
+    @driver.get("localhost:4567")
+  end
 
-def test_correct_user_name
-  element = @driver.find_element :name => "username"
-  element.send_keys "Daniel"
-  element = @driver.find_element :name => "password"
-  element.send_keys "hallo"
-  element.submit
+  def teardown
+    @driver.quit
+  end
 
+  def test_correct_username
+    element = @driver.find_element :name => "username"
+    element.send_keys "Daniel"
+    element = @driver.find_element :name => "password"
+    element.send_keys "hallo"
+    element.submit
 
- # assert(element.text.include?("logged in"), "login was not successfull!")
+    element = @driver.find_element :css => "h1"
+    assert(element.text.include?("Welcome Daniel"), "login was not successfull!")
+    assert_equal(@driver.current_url, "http://localhost:4567/")
+    assert_equal(@driver.title,"item|market")
+  end
 
+  def test_wrong_username
+    element = @driver.find_element :name => "username"
+    element.send_keys "Daniels"
+    element = @driver.find_element :name => "password"
+    element.send_keys "hallo"
+    element.submit
 
-  assert_equal(@driver.current_url, "http://localhost:4567/")
-  assert_equal(@driver.title,"item|market")
+    element = @driver.find_element :id => "table_new"
+    assert(element.text.include?("user doesn't exist!"), "wrong username not detected")
+    assert_equal(@driver.current_url, "http://localhost:4567/login")
+  end
 
-end
+  def test_wrong_password
+    element = @driver.find_element :name => "username"
+    element.send_keys "Daniel"
+    element = @driver.find_element :name => "password"
+    element.send_keys "penis"
+    element.submit
 
-def test_false_user_name
-  element = @driver.find_element :name => "username"
-  element.send_keys "Daniels"
-  element = @driver.find_element :name => "password"
-  element.send_keys "hallo"
-  element.submit
-
-  assert_equal(@driver.current_url, "http://localhost:4567/login")
-
-
-end
-
-
+    element = @driver.find_element :id => "table_new"
+    assert(element.text.include?("wrong password!!"), "wrong password not detected")
+    assert_equal(@driver.current_url, "http://localhost:4567/login")
+  end
 
 end
