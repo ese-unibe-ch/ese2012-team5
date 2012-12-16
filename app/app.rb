46f7c15526c1f064da0d376cc4a21109881ea6bb
Reencode <sudojudo@eml.cc>
2012-12-16 15:25:57 +0100

    cleanup_finished

--- app/app.rb
+++ app/app.rb
@@ -22,6 +22,7 @@ require_relative 'helper/validator.rb'
 require_relative 'helper/checker.rb'
 require_relative 'helper/image_uploader.rb'
 require_relative 'helper/categorizer.rb'
+require_relative 'helper/tooltip.rb'
 
 
 require_relative 'controllers/main.rb'
