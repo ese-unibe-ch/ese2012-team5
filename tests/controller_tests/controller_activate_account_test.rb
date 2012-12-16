054a42f542246433a1aa65c66d84c1e323c16e66
Reencode <sudojudo@eml.cc>
2012-12-16 02:48:45 +0100

    added account activate scenario

--- tests/controller_tests/controller_activate_account_test.rb
+++ tests/controller_tests/controller_activate_account_test.rb
@@ -1,24 +1,69 @@
-require "test/unit"
+require 'rubygems'
+require 'sinatra'
+require 'test/unit'
+require 'bcrypt'
+require 'tilt/haml'
+require 'webget_ruby_secure_random'
+require 'tlsmail'
+require 'require_relative'
+require 'selenium/webdriver'
 
-class ActivateAccount < Test::Unit::TestCase
+require_relative '../../app/controllers/login.rb'
+require_relative '../../app/controllers/main.rb'
+require_relative '../../app/controllers/item.rb'
+require_relative '../../app/controllers/user.rb'
+require_relative '../../app/controllers/register.rb'
+require_relative '../../app/controllers/delete_account.rb'
+require_relative '../../app/controllers/verify.rb'
 
-  # Called before every test method runs. Can be used
-  # to set up fixture information.
+class ControllerActivateAccountTest < Test::Unit::TestCase
+
   def setup
-    # Do nothing
-  end
+    @driver = Selenium::WebDriver.for :firefox
 
-  # Called after every test method runs. Can be used to tear
-  # down fixture information.
+    @base_url = "http://localhost:4567"
+    @driver.manage.timeouts.implicit_wait = 30
+    @verification_errors = []
+    #Testing Securerom Gem
+    mail_extention ="@trash-mail.com"
+    @username = SecureRandom.hex(5)
+    @random_email = @username+mail_extention
+    assert(@random_email.size == mail_extention.size + 10)
+  end
 
   def teardown
-    # Do nothing
+    @driver.quit
+    assert_equal [], @verification_errors
   end
 
-  # Fake test
-  def test_fail
+  def test_register_scenario
+    @driver.get(@base_url + "/")
+    @driver.find_element(:xpath, "(//a[contains(text(),'Register')])[2]").click
+    @driver.find_element(:xpath, "(//input[@name='username'])[2]").clear
+    @driver.find_element(:xpath, "(//input[@name='username'])[2]").send_keys @username
+    @driver.find_element(:id, "email").clear
+    @driver.find_element(:id, "email").send_keys @random_email
+    @driver.find_element(:id, "password").clear
+    @driver.find_element(:id, "password").send_keys "guessM33"
+    @driver.find_element(:id, "password_conf").clear
+    @driver.find_element(:id, "password_conf").send_keys "guessM33"
+    @driver.find_element(:css, "td > input[type=\"submit\"]").click
 
-    # To change this template use File | Settings | File Templates.
-    fail("Not implemented")
+    @driver.get("http://trash-mail.com/index.php")
+    @driver.find_element(:name, "mail").click
+    @driver.find_element(:name, "mail").clear
+    @driver.find_element(:name, "mail").send_keys @username
+    @driver.find_element(:name, "submit").click
+
+    sleep 30 #Seconds to wait for email
+    @driver.find_element(:name, "submit").click
+    @driver.find_element(:link, "itemmarket.mail@gmai...").click
+
+    element = @driver.find_element(:partial_link_text, "verify_account")
+    href = element.attribute("href")
+    @driver.get(href)
+
+    element = @driver.find_element :id => "table_new"
+    assert(element.text.include?("account is now activated"), "account was not activated")
   end
 end
