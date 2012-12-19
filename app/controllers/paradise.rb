e0ef39556b88262c273b33d7be99a3d4e48ff2ba
Urs Zysset <UrsZysset@gmail.com>
2012-12-19 10:25:41 +0100

    DELETE ALL ON MASTER HAHAHAHAHA

--- app/controllers/paradise.rb
+++
@@ -1,8 +0,0 @@
-class Paradise < Sinatra::Application
-
-  # If no controller feels itself responsible for a get query this will lead it into the paradise
-  get %r{(/.*?)+} do
-    redirect '/login'
-  end
-
-end
